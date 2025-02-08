ccfc42548a4ee7518e4117ef06ff233a655f6b1a
Ayush Saini <ayushsaini2004@gmail.com>
2025-02-08 18:41:36 +0530

    Updated webpage titles for all the pages

--- app/(hero)/submit-problem/layout.tsx
+++ app/(hero)/submit-problem/layout.tsx
@@ -1,5 +1,9 @@
 import NavbarWrapper from "@/app/components/NavbarWrapper";
-
+import type {Metadata} from "next";
+export const metadata: Metadata = {
+    title: "Submit Problem - Tensor Bloom",
+    description: "About section of ",
+};
 export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
         <main className="font-work-sans">

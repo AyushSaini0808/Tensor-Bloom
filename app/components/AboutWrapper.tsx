b471ad744e2c1f35b839da0eabebc14e634716be
Ayush Saini <ayushsaini2004@gmail.com>
2025-02-09 12:17:44 +0530

    Removed unnecessary imports

--- app/components/AboutWrapper.tsx
+++
@@ -1,8 +0,0 @@
-// app/navbar-wrapper.tsx
-import { auth } from "@/auth";
-import About from "@/app/(hero)/about/page";
-
-export default async function AboutWrapper() {
-    const session = await auth();
-    return <About session={session} />;
-}
